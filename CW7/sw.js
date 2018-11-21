6af08f15109ad3f90f9bb338f089a62e4174a2d3
sameteraslan <abdulsamet.eraslan@stu.fsm.edu.tr>
2018-11-21 16:59:29 +0300

    Add files via upload

--- CW7/sw.js
+++ CW7/sw.js
@@ -1,34 +1,20 @@
 const CACHE ='JS'
-const FILES = ['/JS/', '/JS/sss/', '/JS/index.html', ...]
 function installCB(e) {
-  e.waitUntil(
-    caches.open(CACHE)
-    .then(cache => cache.addAll(FILES))
-    .catch(console.log)
-  )
+  console.log('install oldu', e.request);
 }
 self.addEventListener('install', installCB)
-
-function cacheCB(e) { //cache first
-  let req = e.request
-  e.respondWith(
-    caches.match(req)
-    .then(r1 => r1 || fetch(req))
-    .catch(console.log)
-  )
-}
-self.addEventListener('fetch', cacheCB)
 
 function save(req, resp) {
   return caches.open(CACHE)
   .then(cache => {
     cache.put(req, resp.clone());
     return resp;
-  }) 
+  })
   .catch(console.log)
 }
 function fetchCB(e) { //fetch first
   let req = e.request
+  console.log('JS', req.url);
   e.respondWith(
     fetch(req).then(r2 => save(req, r2))
     .catch(() => { return caches.match(req).then(r1 => r1) })
