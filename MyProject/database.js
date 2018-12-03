41934fa56802c9c360273b76eadc6ac64a4f08c3
sameteraslan <abdulsamet.eraslan@stu.fsm.edu.tr>
2018-12-03 18:14:04 +0300

    Add files via upload

--- MyProject/database.js
+++ MyProject/database.js
@@ -1,46 +1,4 @@
-const url = "https://sameteraslan.github.io/HomeworksJavaScript/MyProject/text_files/";
-function readData(file) {
-    console.log("readData "+file);
-    if(file.includes("konut.txt")) {
-      fetch(url+file)
-          .then(r => r.text())
-          .then(addKonut);
-    } 
-	else if(file.includes("isyeri.txt")) {
-      fetch(url+file)
-          .then(r => r.text())
-          .then(addIsyeri);
-    }
-	else {
-		console.log("File could not find.")
-	}
-}
-function parseKonut(line) {
-    let b = line.split("\t");
-    return b;
-}
 
-/*function parseCourse(line) {
-	let b = line.split("\t");
-	let name = b[0], time = b[1], date = b[2];
-    let rooms = [];
-    for (let i = 3; i < b.length; i++) 
-		rooms.push(b[i]);
-	//bayrak
-    return new Course(b[0], b[1], b[2], rooms);
-}*/
-function addKonut(txt) {
-    //let msg = txt.length+" chars, ";
-    let a = txt.split("\n");
-    //msg += a.length+" lines, ";
-    for (let s of a) {
-		let std = parseKonut(s);
-		database.konut.set(std[0], [std[1], std[2], std[3], std[4], std[5], std[6]]);
-		//console.log(database.students.get(std.id));
-		//keys.push(std.id); vals.push(std);
-    }
-    //report(msg + database.students.size  + " students");
-}
 /*function addCourses(txt) {
     let msg = txt.length+" chars, ";
     let a = txt.split("\n");
@@ -64,4 +22,4 @@ class Database {
 	}
 }
 
-var database = new Database();
+
