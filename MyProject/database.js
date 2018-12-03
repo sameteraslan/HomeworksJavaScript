26d17141b52a4fe062b52e16442953789c764168
sameteraslan <abdulsamet.eraslan@stu.fsm.edu.tr>
2018-12-03 20:12:42 +0300

    Add files via upload

--- MyProject/database.js
+++ MyProject/database.js
@@ -1,18 +1,3 @@
-
-/*function addCourses(txt) {
-    let msg = txt.length+" chars, ";
-    let a = txt.split("\n");
-    msg += a.length+" lines, ";
-    for (let s of a) {
-		let course = parseCourse(s);
-		database.courses.set(course.name, [course.time, course.date, course.rooms]);
-		//console.log(database.courses.get(course.name));
-		//keys.push(std.id); vals.push(std);
-    }
-    report(msg + database.courses.size  + " students");
-}*/
-
-
 class Database {
 	constructor() {
 		this.konut = new Map();
